9ec98808613eb267e743bb8f295b298c014f900c
Zehra Keremoğlu <zehhrakeremoglu@gmail.com>
2025-06-14 19:26:39 +0300

    Proje iskeleti tamamlandi: API ve Web projeleri eklendi.

---
+++ ProjeYonetim.Sistemi/ProjeYonetim.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjeYonetim.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
